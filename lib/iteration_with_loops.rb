65f94a160d66f1315506ea7599074a67050e94f7
Julio Chazari <julio.chazari@gmail.com>
2020-05-20 04:08:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -12,7 +12,7 @@ def join_nested_strings(src)
     while inner_count < src[count].length do
       
       if src[count][inner_count].class == String
-        single_string_value += src[count][inner_count]
+        single_string_value << src[count][inner_count]
       end
       
       inner_count += 1
